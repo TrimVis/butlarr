e6524a4076e41e62b539db472fb383da75a86f58
Pasquale Jordan <jordanpa@ethz.ch>
2024-02-23 04:24:31 +0100

    More debug output

--- buttlarr/session_database.py
+++ buttlarr/session_database.py
@@ -23,22 +23,31 @@ class SessionDatabase:
     def add_session_entry(self, session_id, value, *, key=None):
         file_name = f"{session_id}.{key}" if key else str(session_id)
         file_path = os.path.join(self.base_path, file_name)
+
+        logger.debug(f"Adding session data for {file_name}")
+        logger.debug(f"Value {value}")
         with open(file_path, mode="wb+") as file:
             pickle.dump(value, file)
 
     def get_session_entry(self, session_id, *, key=None):
         file_name = f"{session_id}.{key}" if key else str(session_id)
         file_path = os.path.join(self.base_path, file_name)
+
+        logger.debug(f"Fetching session data of {file_name}")
         with open(file_path, mode="rb+") as file:
-            return pickle.load(file)
+            result = pickle.load(file)
+            logger.debug(f"Result {result}")
+            return result
 
     def clear_session(self, session_id):
+        file_regex = r"{session_id}\..*"
         all_files = os.listdir(self.base_path)
 
-        file_regex = r"{session_id}\..*"
+        logger.debug(f"Clearing session data of {session_id}")
         for file in all_files:
             if not re.match(file_regex, file):
                 continue
 
             file_path = os.path.join(self.base_path, file)
+            logger.debug(f"Deleting {file}")
             os.remove(file_path)
