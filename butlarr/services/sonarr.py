14eefb892b89a1e5d25ec7e88f2fc14445a08d1a
Lucas K. <lucas.kluber@gmail.com>
2024-06-23 18:46:57 -0300

    add addon fields to sonarr class

--- butlarr/services/sonarr.py
+++ butlarr/services/sonarr.py
@@ -42,6 +42,8 @@ class Sonarr(ExtArrService, ArrService):
         commands: List[str],
         api_host: str,
         api_key: str,
+        name: str = None,
+        addons: List[ArrService] = []
     ):
         self.commands = commands
         self.api_key = api_key
@@ -52,6 +54,10 @@ class Sonarr(ExtArrService, ArrService):
         self.root_folders = self.get_root_folders()
         self.quality_profiles = self.get_quality_profiles()
         self.language_profiles = self.get_language_profiles()
+
+        self.name = name
+        self.supported_addons = []
+        self.addons = addons
 
     @keyboard
     def keyboard(self, state: State, allow_edit=None):
