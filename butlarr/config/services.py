2c4f66f1950d9c114232a0877594c7c41ddf7fa8
Lucas K. <lucas.kluber@gmail.com>
2024-08-21 20:46:47 -0300

    remove comments

--- butlarr/config/services.py
+++ butlarr/config/services.py
@@ -11,7 +11,7 @@ def _constructor(service_type):
         service_module = importlib.import_module(f"butlarr.services.{service_type.lower()}")
         return getattr(service_module, service_type)
     except Exception:
-        assert False, "Could not find a module for that service"
+        assert False, f"Could not find a module for service {service_type}"
 
 # Keep the namespace clean
 def _load_services():
@@ -38,16 +38,12 @@ def _load_services():
         if service_name:
             assert service_name not in named_services, "Different services have been named the same!"
             named_services[service_name] = service
-        print(named_services)
     
     for service in SERVICES:
         logger.info(f"Loading {service.name} addons")
-
         addons = []
         for addon in service.addons:
             addon_service = named_services[addon["service_name"]]
-            print(service.arr_variant)
-            print(addon_service.supported_services)
             if service.arr_variant in (addon_service.supported_services):
                 addons.append(addon_service)
                 logger.info(f"Addon {addon_service.name} loaded")
@@ -56,25 +52,5 @@ def _load_services():
 
         service.addons = addons
         logger.debug(f"{service.name} service loaded Addons: {str(service.addons)}")
-
-            
-            
-        """
-        for supported_service in service.supported_services:
-            
-        service.addons = addons
-
-        for addon in service.addons:
-            for addon_service in SERVICES:
-                if addon_service.name == addon.get("service_name"):
-                    if addon_service.arr_variant in service.supported_addons:
-                        addons.append(addon_service)
-                        logger.info(f"Addon {addon_service.name} loaded")
-                    else:
-                        assert False, f"Unsupported addon service type {addon.get('type')}!"
-                        return False
-        service.addons = addons
-        logger.debug(f"{service.name} service loaded Addons: {str(service.addons)}")
-        """
 
 _load_services()
